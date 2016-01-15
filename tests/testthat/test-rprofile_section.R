86504f50dde30e15ab4c7c8a63d4c9f95596f92e
RobertZK <technoguyrob@gmail.com>
2016-01-14 21:55:01 -0600

    Test rprofile_section constructor

--- tests/testthat/test-rprofile_section.R
+++ tests/testthat/test-rprofile_section.R
@@ -1,5 +1,8 @@
 context("rprofile_section")
 
-test_that("it can create an Rprofile section from one of the allowed types", {
+lapply(c("envvar", "option", "packages", "callback", "code"), function(section_type) {
+  test_that(paste0("it can create a ", section_type, " section"), {
+    expect_is(rprofile_section(section_type), "rprofile_section")
+  })
 })
 
